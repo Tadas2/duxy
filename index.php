479512c18db7fe5b03342c3e1e8f6510384e7671
Tadas2 <tadas.zilinskas74@gmail.com>
2019-02-01 11:37:42 +0200

    pridedu wip page

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>2XY wip</title>
+    </head>
+    <body>
+        <h1>2XY</h1>
+        <h2>Cummmmming soooon</h2>
+        <img src="images/wip.gif" alt="wipimage">
+    </body>
+</html>
