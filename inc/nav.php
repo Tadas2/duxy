06c0e1ace14f99bd69ce1aaceab88347c6926f65
nds <griganed@gmail.com>
2019-02-02 23:54:32 +0200

    pz2a-mat'as done

--- inc/nav.php
+++ inc/nav.php
@@ -1,6 +1,6 @@
 <nav>
     <a href="index.php"><span>Index</span></a>
     <a href="about.php"><span>About</span></a>
-    <a href="pz2a-mat.php"><span>Product Generator</span></a>
+    <a href="pz2a-mat.php"><span>Pz2a-mat</span></a>
     <a href="contact.php"><span>Contact</span></a>
 </nav>
--- pz2a-mat.php
+++ pz2a-mat.php
@@ -5,31 +5,47 @@ $termometras = [
     'NX' => ['pasirodo suvalgytas kivis buvo supuves pomidoras', 'CA ateme telika nors jo nenaudoja', 'nepaemem speed pointu', 'po paskaitu palikau img ir pana rado pornuha', 'vakar geriau todel reikejo vesti paskaita su akiniais nuo saules'],
     'PZ2A' => ['neradau svariu triusiku uzsidejau kojine', 'myzau viesoj vietoj pagavo mentai', 'Skvernelis kandidatuoja i prezidentus', 'jai nera 18os', 'prasileidau nuo Marelio']
 ];
-$term_rand = rand(0, count($termometras) - 1);
-$names = array_keys($termometras);
+$term_keys = array_keys($termometras);
+$temp = rand(0, count($termometras) - 1);
+
+function getClasses($termometras, $temp) {
+    $classes_arr = ['green', 'gold', 'orange', 'brown'];
+
+    for ($i = 0; $i < count($classes_arr); $i++) {
+        if ($i > $temp) {
+            $classes_arr[$i] = 'gray';
+        };
+    }
+
+    return $classes_arr;
+}
+
+$term_classes = getClasses($termometras, $temp);
 ?>
 <html>
     <head>
         <title>DUXY</title>
         <link rel="stylesheet" href="css/main.css">
     </head>
-    <body>
+    <body class='body-pz2a-mat'>
         <?php require 'inc/nav.php'; ?>
         <h1>PZDA-MAT</h1>
         <div class="termometras">
-            <div class="padala-start"></div>
-            <?php foreach ($termometras as $key => $value): ?>
-                <?php if ($key == $names[$term_rand]): ?>
-                    <div class="padala <?php print $key; ?>"><?php print $names[$term_rand]; ?></div>
-                <?php else: ?>
-                    <div class="padala <?php print $key; ?>"></div>
-                <?php endif; ?>
-            <?php endforeach; ?>
+            <div class="green">o</div>
+            <?php for ($i = 0; $i < count($termometras); $i++): ?>
+                <div class="<?php print $term_classes[$i]; ?>">
+                    <?php if ($temp == $i): ?>
+                        <?php print $term_keys[$temp]; ?>
+                    <?php endif; ?>
+                </div>
+            <?php endfor; ?>
         </div>
         <ul>
-            <li>dienos pradžia</li>
-            <?php for ($i = 0; $i <= $term_rand; $i++): ?>
-                <li><?php print $termometras[$names[$i]][rand(0, 4)]; ?></li>
+            <li style="color: green">let's go</li>
+            <?php for ($i = 0; $i <= $temp; $i++): ?>
+                <li style="color: <?php print $term_classes[$i]; ?>">
+                    <?php print $termometras[$term_keys[$i]][rand(0, 4)]; ?>
+                </li>
             <?php endfor; ?>
         </ul>
     </body>
